8a726dc9c003c8a460bda128da9bc4b0912a21aa
Gonçalo Xavier <2190788@my.ipleiria.pt>
2021-04-09 12:23:45 +0100

    Project Criation
    Initial files

---
+++ iPremiumService/iPremiumService/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  iPremiumService
+//
+//  Created by Gonçalo Venâncio Xavier on 09/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TextField("Username (Nome Utilizador)")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iPremiumService/iPremiumService/iPremiumServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPremiumServiceApp.swift
+//  iPremiumService
+//
+//  Created by Gonçalo Venâncio Xavier on 09/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct iPremiumServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
